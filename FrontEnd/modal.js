bf91604cc4ed141fc57d245ef8da38c75fdd012d
Dal Sal <ds@MacBook-Pro-de-Dal.local>
2023-07-12 19:25:40 +0200

    paramétrage close modale

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,8 +1,8 @@
 // TODO wait for the page to load before doing anything --
 // TODO show this only if admin is connected --
 // TODO mask filters if admin is connected --
-// TODO display an empty modal div on click on modify button WITHOUT the pictures BUT WITH the buttons AND the overlay
-// TODO close button of the modal must close the modal and remove the overlay
+// TODO display an empty modal div on click on modify button WITHOUT the pictures BUT WITH the buttons AND the overlay --
+// TODO close button of the modal must close the modal and remove the overlay --
 // TODO change the contents of the modal to display the empty form (Figma `Homepage_edit_2`) when user clicks on add picture button
 // TODO display pictures from the API like in `Homepage_edit_1`
 // TODO display an inactive delete button on each of the pictures
@@ -22,6 +22,18 @@
         e.preventDefault()
         modal.style.visibility = "visible"; 
         overlay.style.visibility = "visible";
-     })
- })
 
+    
+
+    const modalClose = document.getElementById("modalClose");
+        console.log(modalClose)
+    
+    modalClose.addEventListener("click", (e) => {
+        e.preventDefault()
+        console.log("click close")
+        modal.style.visibility = "hidden"; 
+        overlay.style.visibility = "hidden";
+
+    })
+ })
+})
