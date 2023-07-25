f42afc3b8b2346f53ecf235fbd27704eefbdda6e
Dal Sal <ds@MacBook-Pro-de-Dal.local>
2023-07-25 17:34:34 +0200

    affichage des travaux apres suppresion de l'API

--- FrontEnd/galerie.js
+++ FrontEnd/galerie.js
@@ -1,27 +1,38 @@
+
+
 /*
  * Modification du DOM au chargement de la page en faisant appel aux works provenant de l'API lorqu'on l'appel ;
  */
 window.addEventListener("load", async () => {
-
     // we get the gallery container from the HTML
     const gallery = document.querySelector(".gallery");
     // we fetch the works from the API
     const worksAPICall = await fetch("http://localhost:5678/api/works");
     const worksData = await worksAPICall.json();
 
+    function updateGallery() {
+        // vider gallery
+        gallery.innerHTML = '';
+        window.worksData.forEach((work) => {
+            const figure = document.createElement("figure"); // appel d'un element figure dans la div gallery 
+            const image = document.createElement("img"); // appel des images des travaux de la gallery 
+            const figcaption = document.createElement("figcaption"); // appel des titres des travaux de la gallery
+            image.src = work.imageUrl; 
+            image.alt = work.title;
+            figcaption.textContent = work.title;
+            figure.appendChild(image); // appel pour affichage des images de la galerie
+            figure.appendChild(figcaption); // appel pour affichage des titres de la galerie
+            figure.setAttribute("category", work.categoryId);
+            gallery.appendChild(figure); 
+        });
+    }
+
     window.worksData = worksData ; // globaliser worksData pour l'ensemble du code 
     // update the DOM to display the works
-    worksData.forEach((work) => {
-        const figure = document.createElement("figure"); // appel d'un element figure dans la div gallery 
-        const image = document.createElement("img"); // appel des images des travaux de la gallery 
-        const figcaption = document.createElement("figcaption"); // appel des titres des travaux de la gallery
-        image.src = work.imageUrl; 
-        image.alt = work.title;
-        figcaption.textContent = work.title;
-        figure.appendChild(image); // appel pour affichage des images de la galerie
-        figure.appendChild(figcaption); // appel pour affichage des titres de la galerie
-        figure.setAttribute("category", work.categoryId);
-        gallery.appendChild(figure); 
+    updateGallery();
+
+    window.addEventListener("update:gallery", () => {
+        updateGallery();
     });
 });
 
@@ -31,6 +42,8 @@ window.addEventListener("load", async () => {
     const categoriesElements = document.querySelector(".categories");  
     const categoriesAPICall = await fetch("http://localhost:5678/api/categories");
     const categoriesData = await categoriesAPICall.json();
+
+    window.categories = categoriesData;
 
     if (categoriesData.length > 1) {
         const allWorks = document.createElement("button"); // Création du bouton (Tous)
